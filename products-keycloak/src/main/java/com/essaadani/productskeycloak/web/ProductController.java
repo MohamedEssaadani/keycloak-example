0f8305ad937f149a504f1ff4b1ee303773e00843
Mohamed Es-saadani <essaadani80@gmail.com>
2021-10-26 21:36:51 +0100

    'commit'

--- products-keycloak/src/main/java/com/essaadani/productskeycloak/web/ProductController.java
+++ products-keycloak/src/main/java/com/essaadani/productskeycloak/web/ProductController.java
@@ -4,7 +4,11 @@ import com.essaadani.productskeycloak.entities.Supplier;
 import com.essaadani.productskeycloak.repositories.ProductRepository;
 import org.keycloak.KeycloakPrincipal;
 import org.keycloak.KeycloakSecurityContext;
+import org.keycloak.adapters.AdapterDeploymentContext;
+import org.keycloak.adapters.KeycloakDeployment;
+import org.keycloak.adapters.spi.HttpFacade;
 import org.keycloak.adapters.springsecurity.client.KeycloakRestTemplate;
+import org.keycloak.adapters.springsecurity.facade.SimpleHttpFacade;
 import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.PagedModel;
@@ -13,8 +17,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,6 +31,10 @@ public class ProductController{
 
     @Autowired
     private KeycloakRestTemplate keycloakRestTemplate;
+
+    @Autowired
+    private AdapterDeploymentContext adapterDeploymentContext;
+
 
     @GetMapping("/")
     public String index(){
@@ -58,5 +68,15 @@ public class ProductController{
     public String exceptionHandler(Exception e,Model model){
         model.addAttribute("errorMessage", "Probléme d'autorisation");
         return "errors";
+    }
+
+    @GetMapping("/profile")
+    public String profile(RedirectAttributes attributes, HttpServletRequest request, HttpServletResponse response){
+        HttpFacade httpFacade = new SimpleHttpFacade(request, response);
+        KeycloakDeployment deployment = adapterDeploymentContext.resolveDeployment(httpFacade);
+        attributes.addAttribute("referrer", deployment.getResourceName());
+        attributes.addAttribute("referrer_uri", request.getHeader("referer"));
+        return "redirect:"+deployment.getAccountUrl();
+
     }
 }
